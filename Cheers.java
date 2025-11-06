e96fd4aae18fcc286e9d36de64df72379b552b70
amitpom123 <amitpom123@gmail.com>
2025-11-06 15:02:22 +0200

    fixed spacing

--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,25 @@
-//feedback
-
-/*
-Great work boaz!
-*/
-
-
-// Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+                String word = args[0];
+                int runTimes = Integer.parseInt(args[1]);
+                String aOrAn = "";
+                String x = "AEFHILMNORSX";
+                for (int i = 0; i < word.length(); i++) {
+                        for (int j = 0; j < x.length(); j++) {
+                                if (word.charAt(i) != x.charAt(j)) {
+                                        aOrAn = "a ";
+                                } else {
+                                        aOrAn = "an ";
+                                        break;
+                                }
+                        }
+                        System.out.println("Give me " + aOrAn + word.charAt(i) + " : " + word.charAt(i) + "!");
+                }
+                System.out.println("What does that spell?");
+                int a = 0;
+                while (a < runTimes) {
+                        System.out.println(word + "!!!");
+                        a++;
+                }
         }
 }
